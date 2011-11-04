6bf90fe1d798a4c8574fd9de93aa9efc3c55c196
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2011-11-04 00:38:24 -0300

    Adding a separated file to perform the  MySQL connection

---
+++ web/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$file = fopen('../../info.txt','r');
+$url = trim(fgets($file));
+$user = trim(fgets($file));
+$pass = trim(fgets($file));
+fclose($file);
+
+mysql_connect($url,$user, $pass)or die ('Ha fallado la conexión: '.mysql_error().'Valores'.'"'.$user.'"'.$pass.'"');
+mysql_select_db('mukido_adm')or die ('Error al seleccionar la Base de Datos: '.mysql_error());
+
+
+?>
