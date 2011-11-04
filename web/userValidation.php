5e583d1dd29ab30d642eb57950fd213249459ae1
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2011-11-04 00:39:43 -0300

    Modifying the user verification

--- web/userValidation.php
+++ web/userValidation.php
@@ -1,54 +1,51 @@
-<?php session_start();
+<?php
+ session_start();
 
-
-$file = fopen('../../info.txt','r');
-$url = trim(fgets($file));
-$user = trim(fgets($file));
-$pass = trim(fgets($file));
-fclose($file);
-
-mysql_connect($url,$user, $pass)or die ('Ha fallado la conexión: '.mysql_error().'Valores'.'"'.$user.'"'.$pass.'"');
-mysql_select_db('mukido_adm')or die ('Error al seleccionar la Base de Datos: '.mysql_error());
+require_once("connect.php");
 
 function quitar($mensaje)
 {
-	$nopermitidos = array("'",'\\','<','>',"\"");
-	$mensaje = str_replace($nopermitidos, "", $mensaje);
-	return $mensaje;
+   $nopermitidos = array("'",'\\','<','>',"\"");
+   $mensaje = str_replace($nopermitidos, "", $mensaje);
+   return $mensaje;
 }
 if(trim($_POST["username"]) != "" && trim($_POST["password"]) != "")
 {
-	//$usuario = strtolower(quitar($HTTP_POST_VARS["usuario"]));
-	//$password = $HTTP_POST_VARS["password"];
-	$usuario = strtolower(htmlentities($_POST["username"], ENT_QUOTES));
-	$password = sha1($_POST["password"]);
-       
-	$result = mysql_query('SELECT password, username FROM administrador WHERE username=\''.$usuario.'\'');
+   //$usuario = strtolower(quitar($HTTP_POST_VARS["usuario"]));
+   //$password = $HTTP_POST_VARS["password"];
+   $usuario = strtolower(htmlentities($_POST["username"], ENT_QUOTES));
+   $password = sha1($_POST["password"]);
 
-	if($row = mysql_fetch_array($result))
-	{
-		if($row['password'] == $password)
-		{
-			$_SESSION['username'] = $row['username'];
-			echo "
-			<SCRIPT LANGUAGE=\"javascript\">
-			location.href = \"index.php\";
-			</SCRIPT>
-			";
-		}
-		else
-		{
-			echo 'Password incorrecto';
-		}
-	}
-	else
-	{
-		echo 'Usuario no existente en la base de datos';
-	}
-	mysql_free_result($result);
+   $result = mysql_query('SELECT password, username FROM administrador WHERE username=\''.$usuario.'\'');
+
+   if($row = mysql_fetch_array($result)){
+      if($row['password'] == $password){
+         $_SESSION['username'] = $row['username'];
+         echo "<SCRIPT LANGUAGE=\"javascript\">location.href = \"index.php\";</SCRIPT>";
+      }
+      else{
+           echo"<div class=\"main_container\">
+                  <div class=\"notification failure hideit\">
+                     <strong>FALLO: </strong>Contrase&ntilde;a incorrecta.
+                  </div>
+               </div>";
+      }
+   }
+   else{
+         echo"<div class=\"main_container\">
+                <div class=\"notification failure hideit\">
+                   <strong>FALLO: </strong>El usuario no existe.
+                </div>
+             </div>";
+   }
+   mysql_free_result($result);
 }
 else{
-	echo 'Debe especificar un usuario y password';
+     echo"<div class=\"main_container\">
+            <div class=\"notification failure hideit\">
+               <strong>FALLO: </strong>Debe especificar un usuario y una contrase&ntilde;a.
+            </div>
+         </div>";
 }
 mysql_close();
 ?>
